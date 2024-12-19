e8217d068e274c53f5a5fd1e3371459da701e709
piyusss11 <piyusss11@gmail.com>
2024-12-19 18:49:03 +0530

    half done

---
+++ README.md
@@ -0,0 +1,2 @@
+Cloning the gaming website of ZENTRY 
+using Gsap for animations and some tailwind 
---
+++ src/types/types.d.ts
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  id: string;
+  title: string;
+  rightIcon?: ReactNode;
+  leftIcon?: ReactNode;
+  containerClass?: string;
+}
+export interface AnimatedTitleProps {
+  title: string;
+  containerClass?: string;
+}
+export interface BentoTiltProps{
+  children: ReactNode;
+  className?: string
+}
+export interface BentoCardProps{
+  src: string;
+  title: ReactNode;
+  description?: string;
+  isComingSoon?: boolean
+}
